dd52b40a511c572a7d7053c7a6db6041cd35c57e
mountaineerbr <jsonineto@gmail.com>
2021-04-27 10:51:25 -0300

    sync

--- repo/tree.sh
+++ repo/tree.sh
@@ -70,14 +70,20 @@ treef()
 	meta='<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	'
 	#extra styles
-	xtrastyles='.tar  { color: red;  background-color: beige;}
-	h1 { text-transform: uppercase; } 
+	xtrastyles='h1 { text-transform: uppercase; } 
 	img {
 		max-width: 500px;
 		display: block;
 		margin-left: auto;
 		margin-right: auto;
 	}
+	.tar  { color: brown;  background-color: beige;}
+	.pic  { color: blueviolet;}
+	.media  { color: orange;}
+	.doc  { color: indianred;}
+	.txt  { color: crimson;}
+	.pdf  { color: white;  background-color: blue;}
+	.hidden  { background-color: azure;}
 	'
 	#notes
 	notes='To download all files from a directory, try:
@@ -88,14 +94,28 @@ treef()
 	
 	#add package unicode to any .TGZ filenames (U+1F4E6)
 	#and change to class .tar
-	ext='tar.bz2\|tar.gz\|bz2\|rar\|gz\|tar\|tbz2\|tgz\|zip\|Z\|7z\|deb\|zstd\|tar.xz\|txz'
-	extt='tar.bz2|tar.gz|bz2|rar|gz|tar|tbz2|tgz|zip|Z|7z|deb|zstd|tar.xz|txz'
+	ext='tar.bz2\|tar.gz\|bz2\|rar\|gz\|tar\|tbz2\|tgz\|zip\|Z\|7z\|deb\|zstd\|tar.xz\|txz\|iso'
+	extt='tar.bz2|tar.gz|bz2|rar|gz|tar|tbz2|tgz|zip|Z|7z|deb|zstd|tar.xz|txz|iso'
 	if [[ "$(<"$out")" =~ \.($extt) ]]
 	then
 		sed -i -E -e "/\.($extt)\">/I s|class=\"[^\"]*\"|class=\"tar\"|I" "$out"
 		sed -i -E -e "s|\.($ext)<|.\1(📦)<|I" "$out"
 	fi
 
+	#change class of some filetypes
+	pic='jpg|jpeg|png|gif|tiff|webp|bmp|svg|svgz|ico|svg|ppm|tga|tif|xbm|xcf|xpm|xspf|xwd'
+	media='mp3|ogg|oga|flac|m4a|mp4|aac|wma|anx|asf|au|axa|m2a|mid|midi|mpc|ogx|ra|ram|spx|wma|ac3'
+	doc='doc|docx'
+	txt='txt|rtf|conf'
+	pdf=pdf
+	sed -i -E \
+		-e "/\.($pic)\">/I s|class=\"[^\"]*\"|class=\"pic\"|I" \
+		-e "/\.($media)\">/I s|class=\"[^\"]*\"|class=\"media\"|I" \
+		-e "/\.($doc)\">/I s|class=\"[^\"]*\"|class=\"doc\"|I" \
+		-e "/\.($txt)\">/I s|class=\"[^\"]*\"|class=\"txt\"|I" \
+		-e "/\/\.[^\"]+\">/I s|class=\"[^\"]*\"|class=\"hidden\"|I" \
+		-e "/\.($pdf)\">/I s|class=\"[^\"]*\"|class=\"pdf\"|I" \
+		"$out"
 
 	#add meta tags
 	sed -i '/<head.*/ r /dev/stdin' "$out" <<<"$meta"
