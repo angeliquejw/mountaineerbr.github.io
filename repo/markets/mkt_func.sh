e096a74aca46a7db527a094349e7aeb1d1da7435
mountaineerbr <netosj@yahoo.com>
2021-06-09 12:45:16 -0300

    sync

--- repo/markets/mkt_func.sh
+++ repo/markets/mkt_func.sh
@@ -692,10 +692,6 @@ rate()
 }
 
 
-#
-#loss/gain reciprocal or symetrical ratio
-#usage: loss [+-]NUM[%]
-#usage: NUM is an integer and if negative reads as gain
 #loss()
 #{
 #	local u g
@@ -703,7 +699,7 @@ rate()
 #
 #	#check for invalid notations
 #	if [[ "$u" = *[a-zA-Z]* || "$u" != *[0-9]* || ! "${u/[,.-]*}" =~ ^.?.?$ ]]
-#	then echo -n 'err\nusage: loss [+-]NUM[%]' >&2 ;return 1 
+#	then echo -e 'err\nusage: loss [+-]NUM[%]' >&2 ;return 1 
 #	fi
 #
 #	#use zshell maths or bash bc?
@@ -713,12 +709,13 @@ rate()
 #	fi
 #
 #	if [[ "$u" = -* ]]
-#	then printf '%4s: %7.4f %%\n' gain ${u#-} loss ${g#-}
-#	else printf '%4s: %7.4f %%\n' loss $u gain $g
+#	then printf "%4s: %7.${2:-4}f %%\n" gain ${u#-} loss ${g#-}
+#	else printf "%4s: %7.${2:-4}f %%\n" loss $u gain $g
 #	fi
 #}
 #fausto botelho: https://www.youtube.com/watch?v=yY7d6gOIynU
 #formula: 100/(100-loss)
+#
 
 
 #fun with <datahub.io>
